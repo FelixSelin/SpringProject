46b518a4f8078dfda7864f16488fc8b817fe7fb2
felix <tigroff.mike@gmail.com>
2021-05-03 01:38:10 +0300

    Post and get method for sending name

--- src/main/java/com/felixselin/controller/HelloWorldController.java
+++ src/main/java/com/felixselin/controller/HelloWorldController.java
@@ -1,12 +1,30 @@
 package com.felixselin.controller;
 
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
     @GetMapping
-    public String hello() {
+    public String hello(
+            @RequestParam(required = false, defaultValue = "world") String name,
+            Model model
+    ) {
+        model.addAttribute("name", name);
         return "hello";
     }
+
+    @PostMapping
+    public String helloUser(
+            @RequestParam(required = false, defaultValue = "world") String name,
+            Model model
+    ) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+
 }
--- src/main/resources/templates/hello.ftl
+++ src/main/resources/templates/hello.ftl
@@ -1,5 +1,11 @@
 <html>
-    <body>
-        Hello, world
-    </body>
+<body>
+Hello, ${name}
+<div style="margin-top: 10px">
+    <form method="post">
+        <input type="text" name="name" placeholder="Your name"/>
+        <button type="submit">Save</button>
+    </form>
+</div>
+</body>
 </html>
