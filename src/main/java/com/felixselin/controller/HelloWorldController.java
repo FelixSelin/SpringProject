b5f1a3f32ea2b4ad13120f80b1d9687e8dc97920
felix <tigroff.mike@gmail.com>
2021-05-04 00:49:05 +0300

    Saving name in cookies and deleting name button

--- src/main/java/com/felixselin/controller/HelloWorldController.java
+++ src/main/java/com/felixselin/controller/HelloWorldController.java
@@ -1,17 +1,20 @@
 package com.felixselin.controller;
 
-import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class HelloWorldController {
     @GetMapping
     public String hello(
-            @RequestParam(required = false, defaultValue = "world") String name,
+            @CookieValue(name = "username", defaultValue = "world") String name,
             Model model
     ) {
         model.addAttribute("name", name);
@@ -19,12 +22,27 @@ public class HelloWorldController {
     }
 
     @PostMapping
-    public String helloUser(
+    public String setUsername(
+            HttpServletResponse response,
             @RequestParam(required = false, defaultValue = "world") String name,
             Model model
     ) {
+        Cookie cookie = new Cookie("username", name);
+        response.addCookie(cookie);
         model.addAttribute("name", name);
         return "hello";
+    }
+
+    @PostMapping("/logout")
+    public String logout(
+            HttpServletResponse response,
+            Model model
+    ) {
+        Cookie cookie = new Cookie("username", null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+        model.addAttribute("name", "world");
+        return "redirect:/";
     }
 
 }
--- src/main/resources/templates/hello.ftl
+++ src/main/resources/templates/hello.ftl
@@ -1,3 +1,7 @@
+<#assign
+    isNameSetted = username??
+>
+
 <html>
 <body>
 Hello, ${name}
@@ -5,6 +9,11 @@ Hello, ${name}
     <form method="post">
         <input type="text" name="name" placeholder="Your name"/>
         <button type="submit">Save</button>
+    </form>
+</div>
+<div style="margin-top: 10px">
+    <form action="/logout" method="post">
+        <button type="submit">Exit</button>
     </form>
 </div>
 </body>
